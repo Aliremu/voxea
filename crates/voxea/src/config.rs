5ed9c0c491c9fd1e760d794db0df2fc62071df0e
Aliremu <synterxis@gmail.com>
2024-09-03 16:28:53 -0400

    Cargo.toml foramtting

--- crates/voxea/src/config.rs
+++ crates/voxea/src/config.rs
@@ -1,11 +1,11 @@
-use std::sync::OnceLock;
-
-static mut REGISTRY: OnceLock<Config<_>> = OnceLock::new();
-
-pub trait ConfigType {}
-
-pub struct Config<T: ConfigType> {}
-
-pub fn get<T: ConfigType>() -> Config<T> {
-    unsafe {}
-}
+// use std::sync::OnceLock;
+//
+// static mut REGISTRY: OnceLock<Config<_>> = OnceLock::new();
+//
+// pub trait ConfigType {}
+//
+// pub struct Config<T: ConfigType> {}
+//
+// pub fn get<T: ConfigType>() -> Config<T> {
+//     unsafe {}
+// }
